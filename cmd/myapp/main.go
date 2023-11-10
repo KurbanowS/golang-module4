a02bf23361b747995b07b76e4e4208f97bb056e7
Sarcha Gurbanow <kurbanovsarca@gmail.com>
2023-11-10 20:17:07 +0500

    CalcDiscount become method, new function WrOffDebt

--- cmd/myapp/main.go
+++ cmd/myapp/main.go
@@ -1,28 +1,15 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"myapp/internal"
 )
-
-const DEFAULT_DISCOUNT = 500
 
 func main() {
 	cust := internal.NewCustomer("Dmitry", 23, 10000, 1000, true)
 	c := 1000
 	fmt.Println(internal.CalcPrice(*cust, c))
-
-	cust.CalcDiscount = func() (int, error) {
-		if !cust.Discount {
-			return 0, errors.New("Discount not avaible")
-		}
-		result := DEFAULT_DISCOUNT - cust.Debt
-		if result < 0 {
-			return 0, nil
-		}
-		return result, nil
-	}
+	cust.CalcDiscount()
 	fmt.Printf("%+v\n", cust)
-
+	cust.WrOffDebt()
 }
--- internal/customer.go
+++ internal/customer.go
@@ -2,29 +2,52 @@ package internal
 
 import "errors"
 
+const DEFAULT_DISCOUNT = 500
+
 type Customer struct {
-	Name         string
-	Age          int
-	Balance      int
-	Debt         int
-	Discount     bool
-	CalcDiscount func() (int, error)
+	Name     string
+	Age      int
+	balance  int
+	debt     int
+	discount bool
+}
+
+func (c *Customer) WrOffDebt() error {
+	if c.debt >= c.balance {
+		return errors.New("Not possible write off")
+	}
+
+	c.balance -= c.debt
+	c.debt = 0
+
+	return nil
+}
+
+func (c *Customer) CalcDiscount() (int, error) {
+	if !c.discount {
+		return 0, errors.New("Discount not avaible")
+	}
+	result := DEFAULT_DISCOUNT - c.debt
+	if result < 0 {
+		return 0, nil
+	}
+	return result, nil
 }
 
 func NewCustomer(name string, age int, balance int, debt int, discount bool) *Customer {
 	return &Customer{
 		Name:     name,
 		Age:      age,
-		Balance:  balance,
-		Debt:     debt,
-		Discount: discount,
+		balance:  balance,
+		debt:     debt,
+		discount: discount,
 	}
 }
 
 func CalcPrice(c Customer, price int) (int, error) {
-	if c.Debt < price {
+	if c.debt < price {
 		return 0, errors.New("debt is less than price")
 	}
-	sum := c.Debt - price
+	sum := c.debt - price
 	return sum, nil
 }
